071037e2a09e32975dbf49396631d41c9eabfc96
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-29 15:54:50 -0600

    prototype pattern

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-var logger = require('./Logger');
-var Shopper = require('./Shopper');
-var Store = require('./Store');
+var scout_prototype = require('./scout_prototype');
 
-logger.log('starting app...');
+var alex = scout_prototype.clone();
+alex.name = 'Alex Banks';
+alex.addItemToList('slingshot');
 
-var alex = new Shopper('alex', 500)
-var ski_shop = new Store('Steep and Deep Supplies', [
-    {
-        item: 'Downhill Skis',
-        qty: 5,
-        price: 750
-    },
-    {
-        item: 'Knit Hat',
-        qty: 20,
-        price: 5
-    }
-])
+var eve = scout_prototype.clone();
+eve.name = 'Eve Porcello';
+eve.addItemToList('reading light');
 
-logger.log('finished config...');
-
-console.log(`${logger.count} logs total`);
-logger.logs.map(log => console.log(`   ${log.message}`));
+console.log( `${alex.name}: ${alex.shoppingList}` );
+console.log( `${eve.name}: ${eve.shoppingList}` );

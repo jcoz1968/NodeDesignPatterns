10997fa19c2b37f4542a23cfaa10ad19a11fea29
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-29 16:29:15 -0600

    implementing the builder pattern

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var PersonBuilder = require('./PersonBuilder');
 var sue = new PersonBuilder('Sue').makeEmployee().makeManager(60).build();
 var bill = new PersonBuilder('Bill').makeEmployee().makePartTime().build();
 var phil = new PersonBuilder('Phil').makeEmployee().build();
+
 // var sue = new Person('Sue', true, true, 60);
 // var bill = new Person('Bill', true, false, 20);
 // var phil = new Person('Phil', true, false);
@@ -18,3 +19,9 @@ var tabbitha = new PersonBuilder('Tabbitha').withMoney(1000).build();
 
 // var charles = new Person('Charles', false, false, 0, 500, ['jeans', 'sunglasses']);
 // var tabbitha = new Person('Tabbitha', false, false, 0, 1000);
+
+console.log(charles.toString());
+console.log(tabbitha.toString());
+console.log(bill.toString());
+console.log(phil.toString());
+console.log(sue.toString());

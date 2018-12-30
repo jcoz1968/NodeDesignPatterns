bbbeebd3b9d22d90b37cfeb146679a7b095bdd25
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-30 12:35:28 -0600

    decorator pattern

--- Shopper.js
+++ Shopper.js
@@ -1,11 +1,27 @@
-var Person = require('./Person')
+class Shopper {
 
-class Shopper extends Person {
+    constructor(name, account=0) {
+        this.name = name;
+        this.account = account;
+        this.items = [];
+    }
 
-    constructor(name, money=0) {
-        super(name);
-        this.money = money;
-        this.employed = false;
+    purchase(item) {
+        if (item.price > this.account) {
+            console.log(`Cannot afford ${item.name}`);
+        } else {
+            console.log(`Purchasing item ${item.name}`);
+            this.account -= item.price;
+            this.items.push(item);
+        }
+    }
+
+    printStatus() {
+        console.log(`${this.name} has purchased ${this.items.length} items:`);
+        this.items.forEach(item => {
+            console.log(`   * ${item.name} - ${item.price}`);
+        })
+        console.log(`${this.name} has $${this.account.toFixed(2)} remaining.`);
     }
 
 }
--- index.js
+++ index.js
@@ -1,21 +1,12 @@
-var CatalogItem = require('./CatalogItem');
-var CatalogGroup = require('./CatalogGroup');
+var Shopper = require('./Shopper');
+var InventoryItem = require('./InventoryItem');
 
-var boots = new CatalogItem("Leather Boots", 79.99);
-var sneakers = new CatalogItem("Kicks", 39.99);
-var flipFlops = new CatalogItem("California work boots", 19.99);
+var alex = new Shopper('Alex', 100);
 
-var group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops]);
+var walkman = new InventoryItem("Walkman", 29.99);
+var necklace = new InventoryItem("Necklace", 9.99);
 
-var group_food = new CatalogGroup("Food for while you try on clothes", [
-    new CatalogItem("Milkshake", 5.99),
-    new CatalogItem("French Fries", 3.99)
-]);
+alex.purchase(necklace);
+alex.purchase(walkman);
 
-var keychain = new CatalogItem("Key Chain", .99);
-
-var catalog = new CatalogGroup("Clothes and Food", [keychain, group_shoes, group_food]);
-
-console.log(`$${catalog.total}`);
-
-catalog.print();
+alex.printStatus();

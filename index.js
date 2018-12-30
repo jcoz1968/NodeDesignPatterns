c690845111dc08abfd6e778af86e4871586bcf72
Jeffery Cosby <jcoz1968@hotmail.com>
2018-12-30 12:19:40 -0600

    implementing composite

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-var CatalogGroup = require('./CatalogGroup');
 var CatalogItem = require('./CatalogItem');
+var CatalogGroup = require('./CatalogGroup');
 
 var boots = new CatalogItem("Leather Boots", 79.99);
 var sneakers = new CatalogItem("Kicks", 39.99);
 var flipFlops = new CatalogItem("California work boots", 19.99);
 
-var group_shoes = new CatalogGroup('Shoes and Such', [boots, sneakers, flipFlops]);
+var group_shoes = new CatalogGroup("Shoes and Such", [boots, sneakers, flipFlops]);
 
-console.log('boots total: ', `$${boots.total}`);
-console.log( 'shoes total: ', `$${group_shoes.total}` );
+var group_food = new CatalogGroup("Food for while you try on clothes", [
+    new CatalogItem("Milkshake", 5.99),
+    new CatalogItem("French Fries", 3.99)
+]);
 
-boots.print();
-sneakers.print();
+var keychain = new CatalogItem("Key Chain", .99);
 
-group_shoes.print();
+var catalog = new CatalogGroup("Clothes and Food", [keychain, group_shoes, group_food]);
+
+console.log(`$${catalog.total}`);
+
+catalog.print();
